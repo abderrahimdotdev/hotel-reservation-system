1059ab907429d42ef135cde473b4fbc9a36eea21
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 23:06:55 +0100

    Improve Booking record class

--- src/dev/abderrahim/hotel/entities/Booking.java
+++ src/dev/abderrahim/hotel/entities/Booking.java
@@ -4,4 +4,9 @@ import java.util.Date;
 
 public record Booking(User user, Room room, Date checkIn, Date checkOut,int totalPrice) {
 
+    public Booking{
+        // We make a defensive copy to avoid side effects of external modifications to user and room objects
+        user = new User(user.getId(),user.getBalance());
+        room = new Room(room.getRoomNumber(),room.getType(),room.getPrice());
+    }
 }

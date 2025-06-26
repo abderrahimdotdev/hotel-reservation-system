931d5605b4b825bba1e56883eb7fed9cab168885
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 22:53:32 +0100

    Add sample code in Main class

--- src/dev/abderrahim/hotel/Main.java
+++ src/dev/abderrahim/hotel/Main.java
@@ -1,7 +1,34 @@
 package dev.abderrahim.hotel;
 
+import java.util.GregorianCalendar;
+
+import dev.abderrahim.hotel.services.Service;
+import dev.abderrahim.hotel.types.RoomType;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        Service bookinsService = new Service();
         
+		bookinsService.setRoom(1,RoomType.STANDARD,1000);
+		bookinsService.setRoom(2,RoomType.JUNIOR,2000);
+		bookinsService.setRoom(3,RoomType.MASTER,3000);
+
+		bookinsService.setUser(1,5000);
+		bookinsService.setUser(2,10000);
+
+
+		bookinsService.bookRoom(1, 2,new GregorianCalendar(2026,6,30).getTime(),new GregorianCalendar(2026,7,7).getTime());
+		bookinsService.bookRoom(1, 2,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,6,30).getTime());
+		bookinsService.bookRoom(1, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
+
+		bookinsService.bookRoom(2, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,9).getTime());
+		bookinsService.bookRoom(2, 3,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
+
+		bookinsService.setRoom(1,RoomType.MASTER,10000);
+
+		bookinsService.printAll();
+
+		bookinsService.printAllUsers();
     }
 }

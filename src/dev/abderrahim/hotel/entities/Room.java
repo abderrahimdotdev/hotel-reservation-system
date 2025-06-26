488d5b4aaa3ce14d61a506d2e2de9243d602f807
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 22:31:17 +0100

    Add Room entity and RoomType enum

---
+++ src/dev/abderrahim/hotel/entities/Room.java
@@ -0,0 +1,49 @@
+package dev.abderrahim.hotel.entities;
+
+import dev.abderrahim.hotel.types.RoomType;
+
+public class Room {
+    
+    private int roomNumber;
+    private RoomType type;
+    private int pricePerNight;
+
+    public Room(int roomNumber, RoomType type, int pricePerNight) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getPrice() {
+        return pricePerNight;
+    }
+
+    public void setPrice(int newPrice) {
+        if (newPrice < 0) {
+            System.err.println("Price cannot be negative.");
+            return;
+        }
+        this.pricePerNight = newPrice;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int newNumber) {
+        if (newNumber < 0) {
+            System.err.println("Price cannot be negative.");
+            return;
+        }
+        this.roomNumber = newNumber;
+    }
+
+    public RoomType getType() {
+        return type;
+    }
+
+    public void setRoomType(RoomType newType) {
+        this.type = newType;
+    }
+
+}
---
+++ src/dev/abderrahim/hotel/types/RoomType.java
@@ -0,0 +1,5 @@
+package dev.abderrahim.hotel.types;
+
+public enum RoomType {
+    STANDARD, JUNIOR, MASTER;
+}

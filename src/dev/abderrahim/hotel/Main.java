f3a986ae165f8e90658c8a06b7536d06a1abede0
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-27 11:58:29 +0100

    Fix a typo in the name of a variable

--- src/dev/abderrahim/hotel/Main.java
+++ src/dev/abderrahim/hotel/Main.java
@@ -8,27 +8,27 @@ import dev.abderrahim.hotel.types.RoomType;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Service bookinsService = new Service();
+        Service bookingService = new Service();
         
-		bookinsService.setRoom(1,RoomType.STANDARD,1000);
-		bookinsService.setRoom(2,RoomType.JUNIOR,2000);
-		bookinsService.setRoom(3,RoomType.MASTER,3000);
+		bookingService.setRoom(1,RoomType.STANDARD,1000);
+		bookingService.setRoom(2,RoomType.JUNIOR,2000);
+		bookingService.setRoom(3,RoomType.MASTER,3000);
 
-		bookinsService.setUser(1,5000);
-		bookinsService.setUser(2,10000);
+		bookingService.setUser(1,5000);
+		bookingService.setUser(2,10000);
 
 
-		bookinsService.bookRoom(1, 2,new GregorianCalendar(2026,6,30).getTime(),new GregorianCalendar(2026,7,7).getTime());
-		bookinsService.bookRoom(1, 2,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,6,30).getTime());
-		bookinsService.bookRoom(1, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
+		bookingService.bookRoom(1, 2,new GregorianCalendar(2026,6,30).getTime(),new GregorianCalendar(2026,7,7).getTime());
+		bookingService.bookRoom(1, 2,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,6,30).getTime());
+		bookingService.bookRoom(1, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
 
-		bookinsService.bookRoom(2, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,9).getTime());
-		bookinsService.bookRoom(2, 3,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
+		bookingService.bookRoom(2, 1,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,9).getTime());
+		bookingService.bookRoom(2, 3,new GregorianCalendar(2026,7,7).getTime(),new GregorianCalendar(2026,7,8).getTime());
 
-		bookinsService.setRoom(1,RoomType.MASTER,10000);
+		bookingService.setRoom(1,RoomType.MASTER,10000);
 
-		bookinsService.printAll();
+		bookingService.printAll();
 
-		bookinsService.printAllUsers();
+		bookingService.printAllUsers();
     }
 }

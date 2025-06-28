32d51cfd00089f13a8646147e9977b84a3ca87ab
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-28 12:08:32 +0100

    Update comment in Booking.java

--- src/dev/abderrahim/hotel/entities/Booking.java
+++ src/dev/abderrahim/hotel/entities/Booking.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public record Booking(User user, Room room, Date checkIn, Date checkOut,int totalPrice) {
 
     public Booking{
-        // We make a defensive copy to avoid side effects of external modifications to user and room objects
+        // We make a defensive copy to protect from external modifications to user and room objects
         user = new User(user.getId(),user.getBalance());
         room = new Room(room.getRoomNumber(),room.getType(),room.getPrice());
     }
